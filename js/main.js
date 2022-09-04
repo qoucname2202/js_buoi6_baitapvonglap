6109d94eb83ebe4a5feee65a2451aedfadb6f11d
Nam Duong <duongquocnam224400@gmail.com>
2022-09-04 16:12:40 +0700

    Initial build UI project-js_buoi6-Loop

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Change open and close exercises
+const buttons = document.querySelectorAll('.nav button');
+buttons[0].children[0].innerHTML = '📖';
+buttons.forEach((item, idx) => {
+	item.addEventListener('click', e => {
+		item.children[0].innerHTML = '📖';
+		buttons.forEach((res, index) => {
+			if (idx !== index) {
+				res.children[0].innerHTML = '📒';
+			}
+		});
+	});
+});
+// Exercise 1:
+// Exercise 2:
+// Exercise 3:
+// Exercise 4:
+// Exercise 5:
+// Exercise 6:
+// Exercise 7:
